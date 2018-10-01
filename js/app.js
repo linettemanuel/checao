d944ca9acf405645a1ad76b5d77838feabfaae0a
linettemanuel <38947725+linettemanuel@users.noreply.github.com>
2018-10-01 07:25:36 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,6 +2,46 @@ var body=document.getElementsByTagName("body");
 var parent=document.getElementById("parent");
 var child=document.querySelectorAll("#child-div");
 var last = document.querySelector(".child-last");
+var first = document.querySelector(".first-child");
+
+window.onload = () => {
+  parent.children[0].classList.add("fadeIn");
+  addClassToChildren(parent.children[0].children, "fadeIn");
+  changeOpacity(parent.children[0]);
+  console.log(parent.children[0].classList);
+}
+
+
+parent.onscroll = () => {
+  child.forEach ( (item, index) => {
+
+    if ((item.getBoundingClientRect().top < 450) && (item.getBoundingClientRect().top > 0) && (index != 0)){
+      var childrenList = item.children;
+      item.classList.add("appearUp");
+      console.log(item.classList);
+      addClassToChildren(childrenList, "appearUp");
+      changeOpacity(item);
+    }
+  })
+}
+
+function addClassToChildren(e, choosenclass) {
+  for (var i = 0; i < e.length; i++) {
+    e[i].classList.add(choosenclass);
+    var newChildren = e[i].children;
+    addClassToChildren(newChildren, choosenclass);
+  }
+}
+
+function changeOpacity(e) {
+  e.style.opacity = 1;
+  removeAnimation(e,("appearUp", "fadeIn"));
+}
+
+function removeAnimation(e, choosenclass) {
+  console.log(parent.children[0].classList);
+  e.classList.remove(choosenclass);
+}
 
 /*
 parent.onscroll = () => {
